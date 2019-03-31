b7afed772d368fe9ae4b516e5f821bd077736ba1
fcruz219 <fcruz219@gmail.com>
2019-03-31 16:17:04 -0400

    Update bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,12 +2,7 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 var Table = require('cli-table');
 var colors = require('colors/safe');
-var itemname;
-var price;
-var department;
-var stock;
 
-// create the connection information for the sql database
 var connection = mysql.createConnection({
   host: "localhost",
 
@@ -21,14 +16,16 @@ var connection = mysql.createConnection({
   password: "root",
   database: "bamazonDB"
 });
-connection.connect(function(err) {
+
+// create the connection information for the sql database
+connection.connect(function (err) {
   if (err) throw err;
-  // run the start function after the connection is made to prompt the user
+  console.log("connected as id " + connection.threadId + "\n");
   start();
 });
 
 function start() {
-  connection.query("SELECT * FROM products", function(err, res) {
+  connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
     var table = new Table({
       head: ['ID', 'Product Name', 'Department', 'Price', 'Amount In Stock'],
@@ -36,27 +33,79 @@ function start() {
         head: ['blue'],
       }
     });
-    for(var i = 0; i < res.length; i++){
+    for (var i = 0; i < res.length; i++) {
       table.push(
-        [res[i].item_id, res[i].product_name,  res[i].department_name, res[i].price, res[i].stock_inventory]
+        [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_inventory]
       );
     }
     console.log(table.toString());
 
     console.log("-----------------------------------\n");
-    questions();
   });
-}
 
-function questions(){
-  inquirer
-    .prompt([
-      {
-        type: 'input',
-        message: 'What is the ID of the item you wish to purchase?',
-        name: 'input'
-      }
-    ]).then(function(firstquest){
-      
-    })
+    connection.query('SELECT * FROM products', function (err, res) {
+    if (err) throw err
+    // console.log(res)
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          message: 'What is the ID of the item you wish to purchase?',
+          name: 'first',
+          validate: function (value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            return false;
+          }
+        },
+        {
+          type: 'input',
+          message: 'How much would you like to buy?',
+          name: 'amount',
+          validate: function (value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            return false;
+          }
+        }
+      ]).then(function (answer) {
+        var buying = (answer.first) - 1
+        var howMuch = parseInt(answer.amount)
+        // console.log(howMuch)
+        var total = parseFloat(((res[buying].price) * howMuch).toFixed(2));
+
+        if (res[buying].stock_inventory >= howMuch) {
+          connection.query('UPDATE products SET ? WHERE ?', [
+            { stock_inventory: (res[buying].stock_inventory - howMuch) },
+            { item_id: answer.first }
+          ], function (err, results) {
+            if (err) throw err;
+            console.log("\nYour total is $" + total.toFixed(2) + ". Your item(s) will be shipped within 2-4 business days!\n")
+            proceed()
+          });
+        } else{
+          console.log("\nSorry, there's not enough in stock!\n");
+          start()
+
+        }
+
+      })
+
+  })
+}
+function proceed() {
+  inquirer.prompt([{
+    type: "confirm",
+    name: "reply",
+    message: "Would you like to purchase another item?"
+  }]).then(function (ans) {
+    if (ans.reply) {
+      start();
+    } else {
+      console.log("\nSee you soon!\n")
+      connection.end()
+    }
+  })
 }
